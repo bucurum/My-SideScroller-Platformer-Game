b0b89c2dda1cbe036438620c819c2da250e67813
bucurum <bucurum18@posta.pau.edu.tr>
2023-04-19 20:18:41 +0300

    Added character Created grid Added DoubleJump Added playerAbilities Script

---
+++ Assets/[Game]/Scripts/PlayerAbilityTracker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilityTracker : MonoBehaviour
+{
+    public bool canDoubleJump;
+    public bool canDash;
+    public bool canDropBomb;
+}
--- Assets/[Game]/Scripts/PlayerMovementHandler.cs
+++ Assets/[Game]/Scripts/PlayerMovementHandler.cs
@@ -7,12 +7,24 @@ public class PlayerMovementHandler : MonoBehaviour
     [Header("Player")]
     public Rigidbody2D rb;
     [SerializeField] float movementSpeed = 1f;
-    [SerializeField] float jumpForce = 1f;
 
+    
+    [Header("Jump")]
+
+    [SerializeField] float jumpForce = 1f;
+    public Transform groundPoint;
+    private bool isGrounded;
+    public LayerMask groundLayer;
+    private bool canDoubleJump;
+
+    [Header("Abilities")]
+    public PlayerAbilityTracker abilities;
+    
 
     void Start()
     {
         rb.GetComponent<Rigidbody2D>();
+        abilities = GetComponent<PlayerAbilityTracker>();
     }
     void Update()
     {
@@ -27,9 +39,21 @@ public class PlayerMovementHandler : MonoBehaviour
             transform.localScale = Vector3.one;
         }
 
-        if (Input.GetButtonDown("Jump"))
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .1f, groundLayer);
+
+        if (isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            canDoubleJump = true;
         }
+
+        if (Input.GetButtonDown("Jump") && (isGrounded || (canDoubleJump) && abilities.canDoubleJump))
+        {
+            if (!isGrounded)
+            {
+                canDoubleJump = false;
+            }
+            rb.velocity = new Vector2(rb.velocity.y, jumpForce);
+        }
+
     }
 }
