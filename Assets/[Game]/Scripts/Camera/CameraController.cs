66db650f61c8e53667adff7d229bf7b0fe18477a
bucurum <bucurum18@posta.pau.edu.tr>
2023-04-19 21:32:32 +0300

    Added Follow Camera

---
+++ Assets/[Game]/Scripts/Camera/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private PlayerMovementHandler player;
+
+    public BoxCollider2D boundsBox;
+    private float halfHeight;
+    private float halfWidth;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovementHandler>();
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.position = new Vector3(
+            Mathf.Clamp(player.transform.position.x, boundsBox.bounds.min.x + halfWidth, boundsBox.bounds.max.x - halfWidth),
+            Mathf.Clamp(player.transform.position.y, boundsBox.bounds.min.y + halfHeight, boundsBox.bounds.max.y - halfHeight),
+            transform.position.z);
+        }
+        else
+        {
+            player = FindObjectOfType<PlayerMovementHandler>();
+        }
+        
+    }
+}
