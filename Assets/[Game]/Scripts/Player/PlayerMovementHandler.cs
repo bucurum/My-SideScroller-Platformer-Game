295072bd06a6974ce856e93ba8fea2d50b06f60a
bucurum <bucurum18@posta.pau.edu.tr>
2023-04-30 18:20:49 +0300

    Creating Wall tilemap creating wall tag and attach to the walls creating wallslide mechanic creating wallslide animation

--- Assets/[Game]/Scripts/Player/PlayerMovementHandler.cs
+++ Assets/[Game]/Scripts/Player/PlayerMovementHandler.cs
@@ -10,9 +10,14 @@ public class PlayerMovementHandler : MonoBehaviour
     [SerializeField] float movementSpeed = 1f;
     [SerializeField] float crouchSpeed = 1f;
     [SerializeField] Animator animator;
-    [SerializeField] bool canMove;
+    private bool canMove;
 
-
+    [Header("WallSlide")]
+    private bool isWalled;
+    private bool isWallSliding;
+    [SerializeField] float wallSlidingSpeed = 1f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
     
     [Header("Jump")]
 
@@ -40,6 +45,8 @@ public class PlayerMovementHandler : MonoBehaviour
     [SerializeField] float waitAfterDashing;
     
 
+    private float horizontal;
+
     void Start()
     {
         rb.GetComponent<Rigidbody2D>();
@@ -49,94 +56,134 @@ public class PlayerMovementHandler : MonoBehaviour
     }
     void Update()
     {
+        Movement();
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetBool("jump", !isGrounded);   
+    }
+
+    private void Movement()
+    {
         if (canMove)
         {
-            
-           if (dashRechargeCounter > 0)
-            {
-                dashRechargeCounter -= Time.deltaTime;
-            }
-            else
-            {
-                if (Input.GetKeyDown(KeyCode.LeftShift))
-                {
-                    dashCounter = dashTime;
-                    ShowAfterImage();
-                }
-            }
-            
-            if (dashCounter > 0)
-            {
-                dashCounter -= Time.deltaTime;
-                rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
-
-                afterImageCounter -= Time.deltaTime;
-                if (afterImageCounter < 0)
-                {
-                    ShowAfterImage();
-                }
-                dashRechargeCounter = waitAfterDashing;
-            }
-            else
-            {
-                rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y);
-
-                animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-
-                if (rb.velocity.x < 0)
-                {
-                    transform.localScale = new Vector3(-1f, 1f, 1f);
-                }
-                else if (rb.velocity.x > 0)
-                {
-                    transform.localScale = new Vector3(1f,1f,1f);
-                }
-            }
-            
-            isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, groundLayer);
-
-            if (isGrounded)
-            {
-                canDoubleJump = true;
-            }
-
-            if (Input.GetButtonDown("Jump") && (isGrounded || canDoubleJump))
-            {
-                if (!isGrounded)
-                {
-                    canDoubleJump = false;
-                    animator.SetTrigger("doubleJump");
-                }
-                rb.velocity = new Vector2(rb.velocity.y, jumpForce);
-
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                animator.SetTrigger("attack");
-            }
-
-            if (Input.GetKey(KeyCode.LeftControl))
-            {
-                rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * crouchSpeed, rb.velocity.y);
-                animator.SetBool("crouch", true);
-                animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-            }
-            if (Input.GetKeyUp(KeyCode.LeftControl))
-            {
-                rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y);
-                animator.SetBool("crouch", false);
-                animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-            }
-
+            Dash();
+            Jump();
+            Attack();
+            Crouch();
+            WallSlide();
         }
         else
         {
             rb.velocity = Vector2.zero;
         }
-
-        animator.SetFloat("yVelocity", rb.velocity.y);
-        animator.SetBool("jump", !isGrounded);
     }
+
+    private void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * crouchSpeed, rb.velocity.y);
+            animator.SetBool("crouch", true);
+            animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("crouch", false);
+        }
+    }
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("attack");
+        }
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, .2f, groundLayer);
+
+        if (isGrounded)
+        {
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump") && (isGrounded || (canDoubleJump && abilities.canDoubleJump)))
+        {
+            if (!isGrounded)
+            {
+                canDoubleJump = false;
+                animator.SetTrigger("doubleJump");
+            }
+            rb.velocity = new Vector2(rb.velocity.y, jumpForce);
+
+        }
+    }
+
+    private void Dash()
+    {
+        if (dashRechargeCounter > 0)
+        {
+            dashRechargeCounter -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                dashCounter = dashTime;
+                ShowAfterImage();
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            rb.velocity = new Vector2(dashSpeed * transform.localScale.x, rb.velocity.y);
+
+            afterImageCounter -= Time.deltaTime;
+            if (afterImageCounter < 0)
+            {
+                ShowAfterImage();
+            }
+            dashRechargeCounter = waitAfterDashing;
+        }
+        else
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, rb.velocity.y);
+
+            animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+
+            if (rb.velocity.x < 0)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (rb.velocity.x > 0)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+    }
+
+    private void WallSlide()
+    {
+        isWalled = Physics2D.OverlapCircle(wallCheck.position, .2f, wallLayer);
+
+        if (isWalled && !isGrounded)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.x, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+            Debug.Log("wallsliding false");
+        }
+        animator.SetBool("wallSlide",isWallSliding);
+    }
+
+
+
 
     private void ShowAfterImage()
     {
